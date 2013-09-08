6b2cdc36e080f6898c4fb6c7f95ca98fbfc9df2b
Okada Shohei <okashoicircus0@gmail.com>
2013-09-09 01:17:41 +0900

    Use a method 'hover' for the jQuery test.

--- jQuery/test.js
+++ jQuery/test.js
@@ -1,3 +1,8 @@
-$(function(){
-    $(".text1").html("色が変わるよ")
-})
+$(function() {
+    $(".text1").hover(
+        function() {$(this).css("color", "black");
+                    $(this).css("background-color", "yellow")},
+        function() {$(this).css("color", "white");
+                    $(this).css("background-color", "black")}
+    );
+});
